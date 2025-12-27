d4b4c06e11fdb577ea0149ddb08d53f12a71e5e3
Quiniou Christophe <christophe.quiniou@etudiant.univ-rennes1.fr>
2025-12-27 11:02:40 +1100

    Improvements of the Viewer3D with camera movements, select on the cube with box around. better environment (sky)

--- frontend/app/features/configurator/components/Viewer3D.tsx
+++ frontend/app/features/configurator/components/Viewer3D.tsx
@@ -1,8 +1,41 @@
 import { Canvas } from "@react-three/fiber";
 import { observer } from "mobx-react";
 import { useConfiguration } from "../provider/configurationProvider";
+import { CameraControls, ContactShadows, Environment, Grid, Sky } from "@react-three/drei/native";
+import { Select, useSelect, Edges } from "@react-three/drei";
+import { useControls } from 'leva'
+import { useState } from "react";
+
+const Box = observer((props) => {
+    const configuration = useConfiguration();
+    const selected = useSelect(); // Array of THREE.Object3D
+    const isSelected = selected.some((obj) => obj.name === props.id);
+
+    return (
+        <mesh name={props.id}>
+            <boxGeometry args={[configuration.mainProduct().dimensions.length, configuration.mainProduct().dimensions.height, configuration.mainProduct().dimensions.width]} />
+            <meshStandardMaterial color="gold" />
+            <Edges visible={isSelected} scale={1.01} renderOrder={1000} color={"#0066ff"} lineWidth={3}>
+                <meshBasicMaterial transparent depthTest={false} />
+            </Edges>
+        </mesh>
+    );
+})
 
 const Viewer3D = observer(() => {
+    const { gridSize, ...gridConfig } = useControls({
+        gridSize: [10.5, 10.5],
+        cellSize: { value: 0.2, min: 0, max: 10, step: 0.1 },
+        cellThickness: { value: 1, min: 0, max: 5, step: 0.1 },
+        cellColor: '#6f6f6f',
+        sectionSize: { value: 2, min: 0, max: 10, step: 0.1 },
+        sectionThickness: { value: 1.5, min: 0, max: 5, step: 0.1 },
+        sectionColor: '#767676',
+        fadeDistance: { value: 19, min: 0, max: 100, step: 1 },
+        fadeStrength: { value: 1, min: 0, max: 1, step: 0.1 },
+        followCamera: false,
+        infiniteGrid: true
+    })
 
     const configuration = useConfiguration();
     const mainProduct = configuration.products[0] ?? {};
@@ -18,12 +51,16 @@ const Viewer3D = observer(() => {
                 </div>
             </div> */}
             <Canvas>
-                <ambientLight intensity={0.1} />
-                <directionalLight color="white" position={[0, 0, 5]} />
-                <mesh>
-                    <boxGeometry args={[configuration.mainProduct().dimensions.length, configuration.mainProduct().dimensions.height, configuration.mainProduct().dimensions.width]} />
-                    <meshStandardMaterial color="gold" />
-                </mesh>
+                <pointLight position={[10, 10, 10]} />
+                <Environment preset="city" />
+                <ContactShadows frames={1} position={[0, -0.5, 0]} scale={10} opacity={0.4} far={1} blur={2} />
+                <Sky />
+                <Select box multiple onChange={(items) => console.log('Selected:', items)} filter={items => items}>
+                    <Box id="main-product"></Box>
+                </Select>
+
+                <Grid position={[0, -0.01, 0]} args={gridSize} {...gridConfig} />
+                <CameraControls />
             </Canvas>
 
             <div className="absolute left-4 top-4 flex items-center gap-2">
